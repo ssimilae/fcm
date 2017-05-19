fea1809f5ca8c526995a00785b7bdc35967a0d8b
ssimilae <ssimilae@gmail.com>
2017-05-19 14:15:41 +0900

    1
    2

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -10,9 +10,7 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import java.util.Map;
 import java.util.HashMap;
-import java.net.*;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -22,7 +20,7 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "FCMPlugin";
-	 Bitmap bigPicture;
+
     /**
      * Called when message is received.
      *
@@ -31,20 +29,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     // [START receive_message]
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
-		//추가한것 - 메세지와 이미지를 인수로 지정
-        sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getData().get("imgurllink"));
-        //이미지 링크가 제대로 들어 있나 콘솔에 찍어 봄
-        Log.d(TAG, "imgurl: " + remoteMessage.getData().get("imgurllink"));
-
-
-
-
         // TODO(developer): Handle FCM messages here.
         // If the application is in the foreground handle both data and notification messages here.
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
-		/*
         Log.d(TAG, "==> MyFirebaseMessagingService onMessageReceived");
 		
 		if( remoteMessage.getNotification() != null){
@@ -62,8 +50,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 		
 		Log.d(TAG, "\tNotification Data: " + data.toString());
         FCMPlugin.sendPushPayload( data );
-		*/
-
         //sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody(), remoteMessage.getData());
     }
     // [END receive_message]
@@ -73,60 +59,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      *
      * @param messageBody FCM message body received.
      */
-	 //private void sendNotification(String title, String messageBody, Map<String, Object> data) {
-		private void sendNotification(String messageBody, String myimgurl) {
- 
-		//Intent intent = new Intent(this, MainActivity.class);
-        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        
-		Intent intent = new Intent(this, FCMPluginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-		
-		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
-        //이미지 온라인 링크를 가져와 비트맵으로 바꾼다.
-      //  try {
-            URL url = new URL(myimgurl);
-            bigPicture = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-       // } catch (Exception e) {
-            //e.printStackTrace();
-       // }
-
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(getApplicationInfo().icon)
-                .setContentTitle("FCM Push Test")
-                .setContentText("알림탭을 아래로 천천히 드래그 하세요.")
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-
-//                //BigTextStyle
-//                .setStyle(new NotificationCompat.BigTextStyle()
-//                        .setBigContentTitle("FCM Push Big Text")
-//                        .bigText(messageBody))
-
-                //이미지를 보내는 스타일 사용하기
-                .setStyle(new NotificationCompat.BigPictureStyle()
-                        .bigPicture(bigPicture)
-                        .setBigContentTitle("FCM Push Big Text Title")
-                        .setSummaryText(messageBody))
-
-                .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-			notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
- 
-
-
-	 }
-
-
-    private void _sendNotification(String title, String messageBody, Map<String, Object> data) {
-
+    private void sendNotification(String title, String messageBody, Map<String, Object> data) {
         Intent intent = new Intent(this, FCMPluginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		for (String key : data.keySet()) {
@@ -137,7 +70,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(getApplicationInfo().icon)
+                //.setSmallIcon(getApplicationInfo().icon)
+				.setSmallIcon(R.drawable.noti).setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher) )
                 .setContentTitle(title)
                 .setContentText(messageBody)
                 .setAutoCancel(true)
