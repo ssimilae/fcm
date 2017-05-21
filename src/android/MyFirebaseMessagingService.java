940b8a16272284b239755b1e3cdef0cde1817fc0
ssimilae <ssimilae@gmail.com>
2017-05-21 20:58:49 +0900

    1
    2

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -90,6 +90,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 				bigPictureStyle.setBigContentTitle(title);
 				bigPictureStyle.setSummaryText(messageBody);
 				bigPictureStyle.bigPicture(bitmap);
+				bigPictureStyle.bigText(messageBody);
 
 				notificationBuilder = new NotificationCompat.Builder(this)
 				.setSmallIcon(getApplicationInfo().icon)
